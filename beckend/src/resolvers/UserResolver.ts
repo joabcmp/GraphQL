e10f5735637b49376f663ad2e388a7d8eee3b13c
Joabcmp <joabcmp@gmail.com>
2023-12-15 21:24:10 -0300

    apollographQL playground

--- beckend/src/resolvers/UserResolver.ts
+++ beckend/src/resolvers/UserResolver.ts
@@ -1,10 +1,26 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../models/User";
+
+//query: buscar dados
+//mutation: criar, alterar ou deletar
 
 @Resolver()
 export class UserResolver {
+    private data: User[] = [];
     
-    @Query(() => String)
-    async hello() {
-        return 'Hello world'
+    @Query(() => [User])
+    async users() {
+        return this.data;
+    }
+
+    @Mutation(() => User)
+    async createUser(
+        @Arg('name') name: String
+    ) {
+        const user ={id: crypto.randomUUID(), name }
+
+        this.data.push(user)
+
+        return user;
     }
 }
